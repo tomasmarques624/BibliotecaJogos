865336c1c1c1731b3b14030486ce43773978cfdd
tomas marques <marquestomas1@hotmail.com>
2019-11-19 16:26:53 +0000

    a

--- BibliotecaJogos.Database/Tables/tblUsers.sql
+++ BibliotecaJogos.Database/Tables/tblUsers.sql
@@ -4,8 +4,8 @@
 	[username] varchar(50) not null,
 	[password] char(64) not null,
 	[role] char(1) not null,
-	[is_looked] BIT NOT NULL, 
-    [nr_attempts] INT NOT NULL, 
-    [locked_date_time] DATETIME NOT NULL, 
+	[is_looked] BIT DEFAULT 0 NOT NULL, 
+    [nr_attempts] INT NOT NULL DEFAULT 4, 
+    [locked_date_time] DATETIME , 
     constraint [UK_tblUsers_username] unique ([username])
 )
